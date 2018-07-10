367e96829bbe67eb4a0915dfe471defeca215e80
AndreiaSandovalGomes <gomesandreia27@outlook.com>
2018-07-10 11:29:30 -0300

    versao 0.2

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+function myFunction() {
+    document.getElementById("buttonid").disabled = false;
+
+}
+function limiteInput(valor) {
+    quant = 140;
+    total = valor.length;
+    if(total <= quant) {
+        resto = quant - total;
+        document.getElementById('cont').innerHTML = resto;
+    } else {
+        document.getElementById('texto').value = valor.substr(0,quant);
+    }
+}
 var submit =
 document.querySelector("input[type=submit]");
 submit.onclick = function(event) {
