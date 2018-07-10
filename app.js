1b6b921a90a96c04f64231e44a112b75924e6ddb
AndreiaSandovalGomes <gomesandreia27@outlook.com>
2018-07-10 11:30:45 -0300

    versao 0.3

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 function myFunction() {
-    document.getElementById("buttonid").disabled = false;
-
+document.getElementById("buttonid").disabled = false;
 }
 function limiteInput(valor) {
-    quant = 140;
-    total = valor.length;
-    if(total <= quant) {
-        resto = quant - total;
-        document.getElementById('cont').innerHTML = resto;
-    } else {
-        document.getElementById('texto').value = valor.substr(0,quant);
-    }
+  var quant = 140;
+  var total = valor.length;
+  if(total <= quant) {
+  var resto = quant - total;
+    document.getElementById('cont').innerHTML = resto;
+    if (resto <= 20 && resto >= 10)
+       {cont.style = "background-color: yellow";
+    }else if(resto <= 9){
+    cont.style = "background-color: red";
+  }
 }
+  // else if(quant < total){
+  // document.getElementById("buttonid").disabled = true;
+  // }
+}
+
+
 var submit =
 document.querySelector("input[type=submit]");
 submit.onclick = function(event) {
@@ -22,3 +29,12 @@ submit.onclick = function(event) {
   newText.appendChild(newDiv);
   event.preventDefault();
 }
+
+// function time()
+// {
+// today=new Date();
+// h=today.getHours();
+// m=today.getMinutes();
+// document.getElementById('txt').innerHTML=h+":"+m;
+// // setTimeout('time()',500);
+// }
